1f9721b90467271838c3acf196a0e2bdc3cb59d6
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-04-28 03:52:30 -0700

    Update autostart.sh

--- opt/rgbpi/autostart.sh
+++ opt/rgbpi/autostart.sh
@@ -2,4 +2,4 @@
 cd /opt/rgbpi/ui 2> /dev/null
 rm -rf /opt/rgbpi/ui/temp/* 2> /dev/null
 #/usr/bin/python3 /opt/rgbpi/ui/rgbpiui.pyc 2> /dev/null
-/usr/bin/python3 /opt/rgbpi/ui/rgbpiui.pyc 2> /opt/rgbpi/ui/logs/error.log
+/usr/bin/python3.9 /opt/rgbpi/ui/rgbpiui.pyc 2> /opt/rgbpi/ui/logs/error.log
