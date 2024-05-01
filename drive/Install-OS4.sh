1b3b9cfce04621de8bfb570e7ae22e72c6f23e1e
forkymcforkface <36553759+forkymcforkface@users.noreply.github.com>
2024-05-01 15:56:35 -0700

    Update Install-OS4.sh

--- drive/Install-OS4.sh
+++ drive/Install-OS4.sh
@@ -5,8 +5,9 @@ script_dir=$(dirname "$(realpath "$0")")
 for dir in $source_dirs; do
     echo "Copying $dir to /"
     sudo cp -r "$script_dir/$dir" /
+    # Set execute permissions for binaries and .sh files
+    sudo find "/$dir" -type f \( -executable -o -name "*.sh" \) -exec chmod +x {} +
 done
 
 sudo systemctl enable unplug-image.service boot-image.service argon-pwr-off.service argon-btn-fan.service
 7z x /opt/retroarch/cores.7z
-
