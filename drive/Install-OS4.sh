8d66f40c9a5ba38ba653001c46ac147f1a719f83
forkymcforkface <36553759+forkymcforkface@users.noreply.github.com>
2024-05-01 16:38:37 -0700

    Update Install-OS4.sh

--- drive/Install-OS4.sh
+++ drive/Install-OS4.sh
@@ -5,10 +5,13 @@ script_dir=$(dirname "$(realpath "$0")")
 
 for dir in $source_dirs; do
     echo "Copying $dir to /"
-    sudo chmod -R 0777 "$script_dir/$dir"   
+    if [ "$dir" != "boot" ]; then
+        sudo chmod -R 0777 "$script_dir/$dir"
+    fi
     sudo cp -rp "$script_dir/$dir" /
 done
 
 sudo systemctl enable unplug-image.service boot-image.service argon-pwr-off.service argon-btn-fan.service
 7z x /opt/retroarch/cores.7z
+
 reboot
