e23dbd022db963e1bb6a01c3d8a2fe4228003ebe
forkymcforkface <36553759+forkymcforkface@users.noreply.github.com>
2024-05-01 23:36:01 -0700

    Update Install-OS4.sh

--- drive/Install-OS4.sh
+++ drive/Install-OS4.sh
@@ -11,10 +11,16 @@ for dir in $source_dirs; do
     sudo cp -rp "$script_dir/$dir" /
 done
 
+#just in case they somehow came back on :)
+sudo systemctl disable NetworkManager apparmor glamor-test ModemManager rpi-eeprom-update rp1-test triggerhappy NetworkManager-wait-online
+#enable rgbpi os boot and shutdown images
 sudo systemctl enable unplug-image.service boot-image.service argon-pwr-off.service argon-btn-fan.service
-
+#extract cores
 7z x -aoa /opt/retroarch/cores.7z -o/opt/retroarch
+#cleanup install files
 rm /opt/retroarch/cores.7z
+rm -rf /opt/pigpio
+rm -rf /opt/retropie
 rm -rf /root/RetroPie-Setup
 rm -rf /root/RGBPi-Bookworm
 rm -rf /root/RetroPie
